90f721869db1185e5da2730399883fb9c01e04c0
Infinitifall <42463423+Infinitifall@users.noreply.github.com>
2026-05-25 20:13:27 +0530

    remove redundant semicolons

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import json
 import subprocess
-import os
 
 
 INPUT_JSON  = 'data/input.json'
@@ -17,8 +16,8 @@ def getRealSProperty(map_json: dict):
                 map_json['xyz'][map_json['objects'][i]['si'] * 3 + 0],
                 map_json['xyz'][map_json['objects'][i]['si'] * 3 + 1],
                 map_json['xyz'][map_json['objects'][i]['si'] * 3 + 2]
-            ];
-    return map_json;
+            ]
+    return map_json
 
 
 def checkIfUse(map_object: dict):
@@ -41,17 +40,17 @@ def checkIfUse(map_object: dict):
 
 def make_c_input(map_export: dict):
     map_export = getRealSProperty(map_export)
-    return_string = '';  
-    map_objects = map_export['objects'];
-    object_count = len(map_objects);
+    return_string = ''
+    map_objects = map_export['objects']
+    object_count = len(map_objects)
     
     for i in range(object_count):
         if not checkIfUse(map_objects[i]):
-            continue;
+            continue
         
         if 'f' in map_objects[i]:
-            new_f = bin(int(map_objects[i]['f'], 16))[2:];
-            return_string += '0' * (6 - len(new_f)) + f'{new_f}\n';
+            new_f = bin(int(map_objects[i]['f'], 16))[2:]
+            return_string += '0' * (6 - len(new_f)) + f'{new_f}\n'
         else:
             return_string += '111111\n'
         
@@ -74,29 +73,29 @@ def make_c_input(map_export: dict):
         else:
             return_string += '0\n0\n0\n'
 
-    return return_string;
+    return return_string
 
 
 
 def make_c_output(map_export: dict, c_output: str):
     map_export = getRealSProperty(map_export)
-    c_output = c_output.split('\n');
+    c_output = c_output.split('\n')
     real_i = 0
     for i in range(len(map_export['objects'])):
         if not checkIfUse(map_export['objects'][i]):
             continue
         
         map_export['objects'][i]['f'] = hex(int(c_output[real_i], 2))[2:]
-        real_i += 1;
+        real_i += 1
     
-    return map_export;
+    return map_export
 
 
 
 if __name__ == '__main__':
     # convert input.json to input.txt for x program
     json_input = json.load(open(INPUT_JSON, 'r'))
-    c_input = make_c_input(json_input);
+    c_input = make_c_input(json_input)
     open(INPUT_TXT, 'w').write(c_input)
 
     # call c program
@@ -105,6 +104,6 @@ if __name__ == '__main__':
 
     # convert output.txt from c program to output.json
     c_output = open(OUTPUT_TXT, 'r').read()
-    json_output = make_c_output(json_input, c_output);
+    json_output = make_c_output(json_input, c_output)
     json.dump(json_output, open(OUTPUT_JSON, 'w'))
     print('\nOutput is in output.json :)')
